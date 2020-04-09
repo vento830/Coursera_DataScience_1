68a2a8a4563f03fa4bbd9660c63248ee7a97e2a7
Dome <dk.dominik.koch@gmail.com>
2020-04-09 22:45:59 +0200

    First Video

--- Task_1.py
+++ Task_1.py
@@ -1,1 +1,23 @@
-import numpy as np
+####Add numbers###
+x = 1
+y = 2
+print(x + y)
+
+###in function###
+def add_numbers(x, y, z):
+    return x + y + z
+
+print(add_numbers(1,5,2))
+
+###standard value###
+def add_numbers2(x,y, z=None):
+    if (z==None):
+        return x + y
+    else:
+        return x + y + z
+print(add_numbers2(1,2,3))
+print(add_numbers2(1, 4))
+
+###Variabel###
+a = add_numbers2
+print(a(1,2))

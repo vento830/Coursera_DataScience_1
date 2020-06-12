7d991733aebee02abf98efee9fa4b7338f7126e9
Dome <dk.dominik.koch@gmail.com>
2020-06-12 14:40:32 +0200

    Abschluss One Hot Encoder

--- Buch Data Science/Kapitel 4, DataFrame.py
+++ Buch Data Science/Kapitel 4, DataFrame.py
@@ -32,25 +32,24 @@ size_mapping = {'XL' : 3,
                 'M' : 1}
 df['size'] = df['size'].map(size_mapping)
 print(df)
-#inv_size_mapping = {v: k for k, v in size_mapping.items()}
-#df['size'] = df['size'].map(inv_size_mapping)
-#print(df)
+# inv_size_mapping = {v: k for k, v in size_mapping.items()}
+# df['size'] = df['size'].map(inv_size_mapping)
+# print(df)
 class_mapping = {label:idx for idx, label in enumerate(
     np.unique(df['classlabel']))}
 print(class_mapping)
 df['classlabel'] = df['classlabel'].map(class_mapping)
 print(df)
-inv_class_mapping = {v : k for k, v in class_mapping.items()}
+inv_class_mapping = {v: k for k, v in class_mapping.items()}
 df['classlabel'] = df['classlabel'].map(inv_class_mapping)
 print(df)
 class_le = LabelEncoder()
 y = class_le.fit_transform(df['classlabel'].values)
-#print(y)
-#print(class_le.inverse_transform(y))
+# print(y)
+# print(class_le.inverse_transform(y))
 X = df[['color', 'size', 'price']].values
 color_le = LabelEncoder()
-X[:,0] = color_le.fit_transform(X[:,0])
+X[:, 0] = color_le.fit_transform(X[:, 0])
 print(X)
 ohe = OneHotEncoder(categories=0)
-print(ohe.fit_transform(X).toarray())
-
+print(pd.get_dummies(df[['price', 'color', 'size']], drop_first=True))
